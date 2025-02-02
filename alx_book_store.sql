012fc7e103938cf3aa54ae00471a0a6c7347295a
okinyoCoder <okinyoanthony@gmail.com>
2025-02-02 22:12:18 +0300

    Discovering Databases debug 1.1

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,29 +1,30 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 CREATE TABLE IF NOT EXISTS Books(
-    book_id (Primary Key)
+    book_id PRIMARY KEY
     title VARCHAR(130)
-    author_id (Foreign Key referencing Authors table)
     price DOUBLE
     publication_date DATE
+    FOREIGN Key (author_id) REFERENCES Authors (author_id)
+
 );
 CREATE TABLE IF NOT EXISTS Authors(
-    author_id (Primary Key)
+    author_id PRIMARY KEY
     author_name VARCHAR(215)
 );
 CREATE TABLE IF NOT EXISTS Customers(
-    customer_id (Primary Key)
+    customer_id PRIMARY KEY
     customer_name VARCHAR(215)
     email VARCHAR(215)
     address TEXT
 );
 CREATE TABLE IF NOT EXISTS Orders(
-    order_id (Primary Key)
-    customer_id (Foreign Key referencing Customers table)
+    order_id PRIMARY KEY
     order_date DATE
+    FOREIGN KEY customer_id REFERENCES Customers (customer_id)
 );
 CREATE TABLE IF NOT EXISTS Order_Details(
-    orderdetailid (Primary Key)
-    order_id (Foreign Key referencing Orders table)
-    book_id (Foreign Key referencing Books table)
+    orderdetailid PRIMARY KEY
     quantity DOUBLE
+    FOREIGN KEY order_id REFERENCES Orders (order_id)
+    FOREIGN KEY book_id REFERENCES Books (book_id)
 );
