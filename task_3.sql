62baf44e7d46763e2c31634c83ad94872195eea4
okinyoCoder <okinyoanthony@gmail.com>
2025-02-04 09:47:02 +0300

    made changes

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,8 @@
+USE alx_book_store;
+
+-- List all tables in the specified database
+SELECT table_name 
+FROM information_schema.tables 
+WHERE table_schema = 'alx_book_store';
+
 SHOW TABLES;
--- task_6.sql
+++ task_6.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 INSERT INTO Customers (customer_id, customer_name, email, address) VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
