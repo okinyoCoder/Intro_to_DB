19987f8b8593923e8cd73082691cac646b20bae8
okinyoCoder <okinyoanthony@gmail.com>
2025-02-04 09:53:53 +0300

    made changes

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store
+
 USE alx_book_store;
-INSERT INTO Customer (customer_id, customer_name, email, address)
+
+-- Insert a single row into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
